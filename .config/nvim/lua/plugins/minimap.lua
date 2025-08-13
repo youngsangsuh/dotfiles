00f7f075ec5bed3333725d28d857096b9fcc1031
Youngsang Suh <yssuh0427@gmail.com>
2025-08-12 22:59:29 -0700

    add git color

--- .config/nvim/lua/plugins/minimap.lua
+++ .config/nvim/lua/plugins/minimap.lua
@@ -6,5 +6,6 @@ return {
         vim.cmd("let g:minimap_width = 10")
         vim.cmd("let g:minimap_auto_start = 1")
         vim.cmd("let g:minimap_auto_start_win_enter = 1")
+        vim.cmd("let g:minimap_git_colors = 1")
     end,
 }
